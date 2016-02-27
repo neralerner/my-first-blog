6de4f134d7366eb9792ca3a55f081ad25d700f3f
neralerner <neralerner@gmail.com>
2016-02-27 14:03:44 -0500

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf.urls import include, url
+from django.contrib import admin
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include (admin.site.urls)),
+    url(r'', include('blog.urls')),
+
 ]
+
